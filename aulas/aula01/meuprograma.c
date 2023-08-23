ad3dadcf0ed44a08e27fdaca1510a94026b0b580
Marcogazz25 <142800627+Marcogazz25@users.noreply.github.com>
2023-08-23 13:56:05 +0000

    envio para o github

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,16 @@
+/***************************
+  Autor: Marco
+  Data: 23/08/2023
+  
+Programa basico em C
+*******************/
+#include <stdio.h>
+#include <math.h>
+
+//funcao principal
+
+int main(){
+ printf("ola pessoal!\n");
+ printf("Raiz quadrada de 2 é %f\n", sqrt(2)); 
+  return 0;
+}
